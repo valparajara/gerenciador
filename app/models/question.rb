74244c9ddc3312a2c0e883e0f56b801a1602b84c
Valéria Parajara <valeriaparajara@gmail.com>
2015-05-07 16:03:52 -0300

    Refactoring file question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ActiveRecord::Base
 
+  validates :code, :enunciation, :template, presence: true
+
   has_many :alternatives, dependent: :destroy
   accepts_nested_attributes_for :alternatives, limit: 5
 
-  validates :code, :enunciation, :template, presence: true
 end
