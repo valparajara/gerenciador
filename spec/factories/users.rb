5d3a9d587522e0d010c0efcdcea15ca731b88d92
Valéria Parajara <valeriaparajara@gmail.com>
2015-05-08 15:30:09 -0300

    Test fix after the implementation of devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    email 'email@email.com.br'
+    password '12345678'
   end
-
 end
--- spec/features/question_spec.rb
+++ spec/features/question_spec.rb
@@ -1,10 +1,37 @@
 require "rails_helper"
 
-feature "As a user, i want create a question" do
+feature "As a user registered, i want create a question" do
+  let!(:user) { create :user }
+
+  puts :user
+
+  def sign_in(user, password=nil)
+    visit root_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: password || user.password
+
+    click_button "Entrar"
+  end
+
+  context "Given I'm on page login" do
+    scenario "when I digit an e-mail and password valid, then I log successfully" do
+      sign_in(user)
+
+      have_title "Listas de questões"
+    end
+
+    scenario "when I digit e-mail or password invalid, then I see the error" do
+      sign_in(user, 'asd')
+
+      expect(page).to have_content "E-mail ou senha inválidos."
+    end
+  end
 
   context "Given I'm on the questions list" do
+
     scenario "when I click on 'Criar questão', then the question form is displayed" do
-      visit root_path
+      sign_in(user)
 
       click_on "Criar questão"
       have_title "Criar nova questão"
@@ -13,6 +40,7 @@ feature "As a user, i want create a question" do
 
   context "Given I'm on the question form" do
     scenario "when I fill fields all, and I click on  Criar Questão', then the question should registered and I see the question" do
+      sign_in(user)
       visit new_question_path
 
       fill_in 'Código', with: '001'
@@ -35,6 +63,7 @@ feature "As a user, i want create a question" do
     end
 
     scenario "When I not fill one requerid field, and I click on 'Criar Questão', then the question not should registered and I see the error" do
+      sign_in(user)
       visit new_question_path
 
       fill_in 'Código', with: '001'
@@ -57,6 +86,7 @@ feature "As a user, i want create a question" do
     end
 
     scenario "when I not fill a response the alternative and I click on 'Criar Questão', then the question not should registered and I see the error" do
+      sign_in(user)
       visit new_question_path
 
       fill_in 'Código', with: '001'
